8b60870d4cad86b0b0dd42f244a3af196e908577
jiaozongguan <jiao_zg22@163.com>
2017-05-05 21:21:12 +0800

    分页

--- global-fairy-core/src/main/java/org/global/fairy/core/App.java
+++ global-fairy-core/src/main/java/org/global/fairy/core/App.java
@@ -9,5 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        for(int i=0;i<2000000;i++){
+        	System.out.print("dddd");
+        	String testStr="HI"+i;
+        }
     }
 }
